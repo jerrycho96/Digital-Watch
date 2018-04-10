c5309d8dfafff3e010a27b4c1da70bbb6ea7815c
D7703_26 <d7703_26@mac.local>
2018-04-10 15:10:04 +0900

    20180410

--- Digital Watch/ViewController.swift
+++ Digital Watch/ViewController.swift
@@ -9,9 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var timer: UILabel!
+    var num = Timer();
+    @IBOutlet weak var `switch`: UISwitch!
+    @IBOutlet weak var amPm: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+                `switch`.setOn(false, animated: true)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +25,33 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func startTime() {
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm:ss"
+        timer.text = formatter.string(from: date)
+        formatter.dateFormat = "a"
+        amPm.text = formatter.string(from: date)
+        
 
-
-}
+    }
+    
+    @IBAction func startSwitch(_ sender: Any) {
+        if `switch`.isOn == true {
+            //            myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+            
+            // closure 사용
+            num = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {(num) in
+                self.startTime()
+            })
+            
+        } else {
+            num.invalidate()
+            
+        }
+    }
+        
+    }
+    
 
